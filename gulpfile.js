166e6f83beab8dee9cf506e34248f13c722cfe10
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-03-02 12:42:22 +0000

    create task for min.css

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,28 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("app/*.html").on('change', browserSync.reload);
 });
+
+var config = {
+  srcDir: './src',
+  sassPattern: 'sass/**/*.scss'
+};
+
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function () {
+  return gulp.src([config.srcDir+'/sass/libs.scss', config.srcDir+'/sass/style.scss', config.srcDir+'/sass/responsive.scss'])
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('main.css'))
+  .pipe(cleanCSS())
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./design/okay_shop/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(config.srcDir+'/'+config.sassPattern, ['sass'])
+});
