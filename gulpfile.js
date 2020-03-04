c32f24645c1940c023c70322757c1e99f81cf9fd
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-03-04 13:57:29 +0000

    feat: add gulp-sass in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,25 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-
-gulp.task('hello', function(done) {
-  console.log('Привет мир!');
-  done();
-});
+const sass= require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("app/*.html").on('change', browserSync.reload);
-});
-
-var config = {
-  srcDir: './src',
-  sassPattern: 'sass/**/*.scss'
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./Js/*.js").on('change', browserSync.reload);
 };
 
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  sourcemaps = require('gulp-sourcemaps'),
-  concat = require('gulp-concat'),
-  cleanCSS = require('gulp-clean-css');
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
 
-gulp.task('sass', function () {
-  return gulp.src([config.srcDir+'/sass/libs.scss', config.srcDir+'/sass/style.scss', config.srcDir+'/sass/responsive.scss'])
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(concat('main.css'))
-  .pipe(cleanCSS())
-  .pipe(sourcemaps.write('.'))
-  .pipe(gulp.dest('./design/okay_shop/css'));
-});
-
-gulp.task('watch', function () {
-  gulp.watch(config.srcDir+'/'+config.sassPattern, ['sass'])
-});
+exports.serve = bs;
