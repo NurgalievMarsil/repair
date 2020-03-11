59eba00aa13dac5be1d23093bd675bf058b7bf83
NurgalievMarsil <60772410+NurgalievMarsil@users.noreply.github.com>
2020-03-11 16:08:05 +0000

    Add files via upload

--- Js/script.js
+++ Js/script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   
-    const modal = document.querySelector(".modal"),
-        switchModal = () => modal.classList.toggle('modal--visible');
-  
-    document.body.addEventListener('click', ev => {
-        const {target} = ev;
-        if (target.matches('[data-toggle=modal], .modal__close')) {
-            switchModal();
-        } else if (!target.closest('.modal') && modal.classList.contains('modal--visible')) {
-            switchModal();
-        }
-    });
-  
-    document.addEventListener('keydown', e => {
-      e.keyCode === 27 && modal.classList.contains('modal--visible') ? switchModal() : false;
+  const modal = document.querySelector(".modal"),
+      switchModal = () => modal.classList.toggle('modal--visible');
+
+  document.body.addEventListener('click', ev => {
+      const {target} = ev;
+      if (target.matches('[data-toggle=modal], .modal__close')) {
+          switchModal();
+      } else if (!target.closest('.modal') && modal.classList.contains('modal--visible')) {
+          switchModal();
+      }
   });
-  });
+
+  document.addEventListener('keydown', e => {
+    e.keyCode === 27 && modal.classList.contains('modal--visible') ? switchModal() : false;
+});
+});
