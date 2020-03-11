1c4d4d4b8edc656f98cf705ce24a1644b7c0fd49
NurgalievMarsil <ChechenskiyArc@yandex.ru>
2020-03-11 19:25:40 +0000

    fsdf

--- Js/script.js
+++ Js/script.js
@@ -1,20 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  const modal = document.querySelector(".modal");
-  const dark = document.querySelector(".dark");
-  const modalBtn = document.querySelectorAll("[data-toggle=modal]");
-  const closeBtn = document.querySelector(".modal__close");
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-    dark.classList.toggle('dark--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+document.addEventListener("DOMContentLoaded", function (event) {
+  
+  const modal = document.querySelector(".modal"),
+      switchModal = () => modal.classList.toggle('modal--visible');
+
+  document.body.addEventListener('click', ev => {
+      const target = ev.target;
+      if (target.matches('[data-toggle=modal], .modal__close')) {
+          switchModal();
+      } else if (!target.closest('.modal') && modal.classList.contains('modal--visible')) {
+          switchModal();
+      }
   });
-  dark.addEventListener('click', switchModal);
-  closeBtn.addEventListener('click', switchModal);
+
   document.addEventListener('keydown', function (e) {
-    if (e.keyCode === 27 && modal.classList.contains('modal--visible')) {
-        switchModal();
-    }
+      if (e.keyCode === 27 && modal.classList.contains('modal--visible')) switchModal();
   });
+
 });
